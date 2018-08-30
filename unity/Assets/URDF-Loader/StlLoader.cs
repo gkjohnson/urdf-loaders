ea6e988af3053e33f3ef3ef071eaa9cf93c102de
Garrett Johnson <garrett.kjohnson@gmail.com>
2018-08-29 19:56:05 -0700

    Add ASCII STL support

--- unity/Assets/URDF-Loader/StlLoader.cs
+++ unity/Assets/URDF-Loader/StlLoader.cs
@@ -8,8 +8,8 @@ using System.Text;
 // Loads the Binary STL format
 public class StlLoader {
 
-    const int MAX_VERTEX_COUNT = 65000;    
-    
+    const int MAX_VERTEX_COUNT = 65000;
+
     // Returns an array of meshes used to build up
     // the given STL
     public static Mesh[] Load(string fileName) {
@@ -19,28 +19,110 @@ public class StlLoader {
     }
 
     public static Mesh[] Load(byte[] bytes) {
-
-        BinaryReader reader = new BinaryReader(new MemoryStream(bytes));
-
+        
         // Read and throw out the header
-        byte[] fileTypeBytes = reader.ReadBytes(5);
-        string fileType = Encoding.ASCII.GetString(fileTypeBytes);
+        string fileType = Encoding.ASCII.GetString(bytes, 0, 5);
 
         if (fileType == "solid") {
 
-            Debug.LogWarning("ASCII STL files are not supported.");
-            return null;
+            string content = Encoding.ASCII.GetString(bytes);
+            string[] lines = content.Split('\n');
+            return LoadAscii(lines);
 
         } else {
 
-            reader.ReadBytes(75);
-            return LoadBinary(reader);
+            return LoadBinary(bytes);
 
         }
         
     }
 
-    static Mesh[] LoadBinary(BinaryReader reader) {
+    static Mesh[] LoadAscii(string[] lines) {
+
+        List<Mesh> meshes = new List<Mesh>();
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        int currTri = 0;
+
+        for(int i = 1; i < lines.Length; i ++) {
+
+            // facet normal x y z
+            string line = lines[i].Trim();
+            string[] tokens = line.Split(' ');
+
+            if (tokens[0] == "endsolid") break;
+
+            Vector3 normal = Vector3.zero;
+            if (tokens.Length == 5 && tokens[1] == "normal") {
+
+                normal = ReadAsciiVector(tokens[2], tokens[3], tokens[4]);
+
+                Debug.Log(normal);
+
+            }
+
+            normals.Add(normal);
+            normals.Add(normal);
+            normals.Add(normal);
+
+            // outer loop
+            i++;
+
+            // iterate over the vertices
+            for(int j = 0; j < 3; j ++) {
+
+                // vertex x y z
+                i++;
+                string vline = lines[i].Trim();
+                string[] vtokens = vline.Split(' ');
+
+                Vector3 vertex = ReadAsciiVector(vtokens[1], vtokens[2], vtokens[3]);
+                vertices.Add(vertex);
+
+            }
+
+            // get to endloop
+            while (!lines[i].Contains("endloop")) i++;
+
+            // endfacet
+            i++;
+
+            // Add vertices in reverse order because of Unity frame conversion
+            triangles.Add(currTri + 2);
+            triangles.Add(currTri + 1);
+            triangles.Add(currTri + 0);
+            currTri += 3;
+
+            if (vertices.Count > MAX_VERTEX_COUNT - 1) {
+
+                Mesh newMesh = ToMesh(vertices, normals, triangles);
+                meshes.Add(newMesh);
+
+                vertices.Clear();
+                normals.Clear();
+                triangles.Clear();
+                currTri = 0;
+
+            }
+
+        }
+
+        if (vertices.Count > 0) {
+
+            Mesh newMesh = ToMesh(vertices, normals, triangles);
+            meshes.Add(newMesh);
+
+        }
+
+        return meshes.ToArray();
+
+    }
+
+    static Mesh[] LoadBinary(byte[] bytes) {
+
+        BinaryReader reader = new BinaryReader(new MemoryStream(bytes));
+        reader.ReadBytes(80);
 
         uint trianglesCount = reader.ReadUInt32();
         Mesh[] meshes = new Mesh[Mathf.CeilToInt(trianglesCount * 3.0f / MAX_VERTEX_COUNT)];
@@ -52,15 +134,15 @@ public class StlLoader {
         int meshIndex = 0;
 
         for (int i = 0; i < trianglesCount; i++){
-            Vector3 n = ReadBinaryVectory(reader);
+            Vector3 n = ReadBinaryVector(reader);
 
             normals.Add(n);
             normals.Add(n);
             normals.Add(n);
 
-            Vector3 v0 = ReadBinaryVectory(reader);
-            Vector3 v1 = ReadBinaryVectory(reader);
-            Vector3 v2 = ReadBinaryVectory(reader);
+            Vector3 v0 = ReadBinaryVector(reader);
+            Vector3 v1 = ReadBinaryVector(reader);
+            Vector3 v2 = ReadBinaryVector(reader);
 
             vertices.Add(v0);
             vertices.Add(v1);
@@ -75,31 +157,44 @@ public class StlLoader {
             // Attribute byte count
             // assume unused
             reader.ReadUInt16();
-            
-            if (vertices.Count <= MAX_VERTEX_COUNT - 1) continue;
-                        
-            meshes[meshIndex++] = ToMesh(vertices, normals, triangles);
-            
-            vertices.Clear();
-            normals.Clear();
-            triangles.Clear();
-            currTri = 0;
+
+            if (vertices.Count > MAX_VERTEX_COUNT - 1) {
+
+                meshes[meshIndex++] = ToMesh(vertices, normals, triangles);
+
+                vertices.Clear();
+                normals.Clear();
+                triangles.Clear();
+                currTri = 0;
+
+            }
+
         }
 
-        if (vertices.Count > 0) meshes[meshIndex++] = ToMesh(vertices, normals, triangles);
+        if (vertices.Count > 0) {
 
+            meshes[meshIndex++] = ToMesh(vertices, normals, triangles);
+
+        }
+        
         return meshes;
 
     }
     
     /* Utilities */
     // Read a vector from the binary reader
-    static Vector3 ReadBinaryVectory(BinaryReader br) {
+    static Vector3 ReadBinaryVector(BinaryReader br) {
 
         Vector3 v = new Vector3(br.ReadSingle(), br.ReadSingle(), br.ReadSingle());
 
         // convert from STL to Unity frame
         return new Vector3(-v.y, v.z, v.x);
+
+    }
+
+    static Vector3 ReadAsciiVector(string x, string y, string z) {
+
+        return new Vector3(-float.Parse(y), float.Parse(z), float.Parse(x));
 
     }
 
