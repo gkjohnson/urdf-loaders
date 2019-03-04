4514e2025be03b2bb754b08fa3ba9ca2faedbef8
Garrett Johnson <garrett.kjohnson@gmail.com>
2019-03-04 12:05:01 -0800

    Update URDFLoader.d.ts

--- javascript/src/URDFLoader.d.ts
+++ javascript/src/URDFLoader.d.ts
@@ -1,21 +1,19 @@
 import { LoadingManager, Object3D } from 'three';
 import { URDFRobot } from './URDFClasses';
 
+declar type MeshLoadFunc = function(url: string, manager: LoadingManager, onLoad: function(mesh: Object3D): void);
 declare type URDFLoaderOptions = {
 
     packages?: string | { [key: string]: string },
-    loadMeshCb?: function(
-        url: string,
-        manager: LoadingManager,
-        onLoad: function(mesh: Object3D): void
-    ),
+    loadMeshCb?: MeshLoadFunc,
     workingPath?: string,
     fetchOptions?: object
 
 };
-class URDFLoader {
+export default class URDFLoader {
 
     manager: LoadingManager;
+    defaultMeshLoader: MeshLoadFunc;
     constructor(manager?: Manager);
     load(url: string, function(robot: URDFRobot): void, options?: URDFLoaderOptions): void;
     parse(content: string, options?: URDFLoaderOptions): URDFRobot;
