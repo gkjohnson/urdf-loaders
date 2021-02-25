898f8596d5e24b3786e326bd7fc1d4ea4f61465c
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-02-25 10:20:10 -0800

    Fix manipulation element bug

--- javascript/src/urdf-manipulator-element.js
+++ javascript/src/urdf-manipulator-element.js
@@ -109,14 +109,14 @@ class URDFManipulator extends URDFViewer {
         dragControls.onHover = joint => {
 
             highlightLinkGeometry(joint, false);
-            this.dispatchEvent(new CustomEvent('joint-mouseout', { bubbles: true, cancelable: true, detail: joint.name }));
+            this.dispatchEvent(new CustomEvent('joint-mouseover', { bubbles: true, cancelable: true, detail: joint.name }));
             this.redraw();
 
         };
         dragControls.onUnhover = joint => {
 
             highlightLinkGeometry(joint, true);
-            this.dispatchEvent(new CustomEvent('joint-mouseover', { bubbles: true, cancelable: true, detail: joint.name }));
+            this.dispatchEvent(new CustomEvent('joint-mouseout', { bubbles: true, cancelable: true, detail: joint.name }));
             this.redraw();
 
         };
