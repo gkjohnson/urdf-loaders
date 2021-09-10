5874e8d498e023e2ff97ac0961506c7df00487ca
Garrett Johnson <garrett.kjohnson@gmail.com>
2021-09-10 10:47:24 -0700

    add backward compatible redirect

---
+++ javascript/example/src/redirect.js
@@ -0,0 +1,10 @@
+// redirect from example to example/bundle for demo
+// link backward compatibility
+const url = new URL(location.href);
+const tokens = url.pathname.split(/[\\/]/g);
+const filename = tokens.pop();
+const parentDirectory = tokens[ tokens.length - 1 ];
+if (parentDirectory !== 'bundle') {
+    url.pathname = tokens.join('/') + '/';
+    window.location.href = new URL('bundle/' + filename, url.toString());
+}
