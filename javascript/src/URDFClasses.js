7135f22cc851e8745b219c7e92000de634ed0b2d
Noah Wardlow <noah.wardlow@picknik.ai>
2023-10-05 15:49:43 -0600

    missed deletion of line

--- javascript/src/URDFClasses.js
+++ javascript/src/URDFClasses.js
@@ -219,7 +219,6 @@ class URDFJoint extends URDFBase {
                 this.position.copy(this.origPosition);
                 const tempAxis = this.axis.clone().applyEuler(this.rotation);
                 this.position.addScaledVector(tempAxis, pos);
-                this.position.addScaledVector(this.axis, pos);
 
                 if (this.jointValue[0] !== pos) {
 
