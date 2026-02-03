60ecff64aab5c16e95d46ec0c302bf88298fbaa0
Garrett Johnson <garrett.kjohnson@gmail.com>
2026-02-03 09:59:27 +0900

    Fix build

--- javascript/vite.config.js
+++ javascript/vite.config.js
@@ -2,25 +2,26 @@ import path from 'path';
 import fs from 'fs';
 import { fileURLToPath } from 'url';
 
-const __dirname = path.dirname( fileURLToPath( import.meta.url ) );
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const input = {};
+const files = fs.readdirSync(path.resolve(__dirname, './example/')).filter(p => /\.html$/.test(p));
+files.forEach(file => {
+    input[file] = path.resolve(__dirname, `./example/${ file }`);
+});
 
 export default {
 
-	// Serve from urdf-loaders root so ../../../urdf/ paths work correctly
-	root: path.resolve( __dirname, '../' ),
-	base: '',
-	build: {
-		sourcemap: true,
-		outDir: path.resolve( __dirname, './example/bundle/' ),
-		rollupOptions: {
-			input: fs
-				.readdirSync( path.resolve( __dirname, './example/' ) )
-				.filter( p => /\.html$/.test( p ) )
-				.map( p => path.resolve( __dirname, `./example/${ p }` ) ),
-		},
-	},
-	server: {
-		open: '/javascript/example/index.html',
-	},
+    // Serve from urdf-loaders root so ../../../urdf/ paths work correctly
+    root: path.resolve(__dirname, '../'),
+    base: '',
+    build: {
+        sourcemap: true,
+        outDir: path.resolve(__dirname, './example/bundle/'),
+        rollupOptions: { input },
+    },
+    server: {
+        open: '/javascript/example/index.html',
+    },
 
 };
